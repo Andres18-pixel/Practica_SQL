af12485c54408e166abc579c17b8d51786c2e71d
Erickson Andrés López <ericksonlopez@uamv.edu.ni>
2026-05-28 23:11:22 -0600

    Subiendo mi trabajo a GitHub

---
+++ Práctica SQL.sql
@@ -0,0 +1,11 @@
+USE master;
+GO
+
+IF DB_ID(N'bd_universidad') IS NOT NULL
+BEGIN
+    ALTER DATABASE bd_universidad SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE bd_universidad;
+END
+GO
+
+
